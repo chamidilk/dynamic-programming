098f4389e10dd8f5ce4f5a6f376c70762f33f0b9
chamidilk <chamidilk@gmail.com>
2022-06-19 06:28:14 +0530

    add tests for fibnoacci add springboot testing change to java17

--- src/test/java/com/dilshan/dynamicprogramming/FibonacciTest.java
+++ src/test/java/com/dilshan/dynamicprogramming/FibonacciTest.java
@@ -1,9 +1,38 @@
 package com.dilshan.dynamicprogramming;
 
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
 public class FibonacciTest {
 
+//    private final Fibonacci fibonacci = new Fibonacci();
 
+    @Test
+    public void testFibonacci50(){
+        // given
+        int n = 50;
+
+        // when
+        long result = Fibonacci.fib(n, new HashMap<>());
+
+        // then
+        Assertions.assertThat(result).isEqualTo(12586269025L);
+    }
+
+    @Test
+    public void testFibonacci10(){
+        // given
+        int n = 10;
+
+        // when
+        long result = Fibonacci.fib(n, new HashMap<>());
+
+        // then
+        Assertions.assertThat(result).isEqualTo(55L);
+    }
 
 
 }
